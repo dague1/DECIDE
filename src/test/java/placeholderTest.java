feabae0447fcd4e9b8f27961680cc2ba1d94b701
David Ljunggren <davidxljunggren@gmail.com>
2023-01-24 16:27:55 +0100

    JUnit 5 imported and mock test created

---
+++ src/test/java/placeholderTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class placeholderTest {
+    @Test
+   public void testMe() {
+       int x = 1;
+       int y = 2;
+       int res = 3;
+        assertEquals(x+y, res);
+    }
+
+}
