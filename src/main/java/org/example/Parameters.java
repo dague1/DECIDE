7f5351cc8f22517241ea9d8d50c18be768c0779c
David Ljunggren <davidxljunggren@gmail.com>
2023-01-26 01:28:30 +0100

    #18: Constructor created. Followed IntelliJs advice and used the compact form rather than the conventional this.attr = attr form

---
+++ src/main/java/org/example/Parameters.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * This class contains the Parameters data.
+ * @author Group 4
+ */
+public record Parameters(double length1, double radius1, double epsilon, double area1, int qPts, int quads, double dist,
+                         int nPts, int kPts, int aPts, int bPts, int cPts, int dPts, int ePts, int fPts, int gPts,
+                         double length2, double area2) {
+
+    /**
+     * Constructor for parameters
+     */
+    public Parameters {
+    }
+
+
+}
