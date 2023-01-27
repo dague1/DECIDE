a41381318984b2e23b7097dc5524d377166f7cc6
chenyi <whale0827@icloud.com>
2023-01-27 19:02:46 +0100

    #3: Added Unit test for checkLIC1
    Added three points unit test and four points unit test, all test cases are passed

--- src/test/java/org/example/CMVCalculatorTest.java
+++ src/test/java/org/example/CMVCalculatorTest.java
@@ -16,4 +16,52 @@ public class CMVCalculatorTest {
         assertFalse(CMVCalculator.checkLIC0(points, 10.0d));
         assertFalse(CMVCalculator.checkLIC0(points, -1.0d));
     }
+
+    @Test
+    public void TestBasicThreePointsLIC1(){
+        // check three identical points
+        float[] point1 = new float[]{ 1.0f, 1.0f};
+        float[][] points = new float[][]{point1, point1, point1};
+        assertFalse(CMVCalculator.checkLIC1(points, 1.0d));
+        // check invalid RADIUS1
+        assertFalse(CMVCalculator.checkLIC1(points, -1.0d));
+
+        // check three collinear points
+        float[] point2 = new float[]{ 1.0f, 0.0f};
+        float[] point3 = new float[]{ 1.0f, 2.0f};
+        points = new float[][]{point1, point2, point3};
+        assertFalse(CMVCalculator.checkLIC1(points, 1.0d));
+        assertFalse(CMVCalculator.checkLIC1(points, 1.5d));
+        assertTrue(CMVCalculator.checkLIC1(points, 0.8d));
+
+        // check 2 points (on boundary) circle
+        point1 = new float[]{0.2f, 1.0f};
+        points[0] = point1;
+        assertFalse(CMVCalculator.checkLIC1(points, 1.0d));
+        assertFalse(CMVCalculator.checkLIC1(points, 1.5d));
+        assertTrue(CMVCalculator.checkLIC1(points, 0.8d));
+
+        // check 3 points (on boundary) circle
+        point1 = new float[]{0.0f, 1.0f};
+        points[0] = point1;
+        assertFalse(CMVCalculator.checkLIC1(points, 1.0d));
+        assertFalse(CMVCalculator.checkLIC1(points, 1.5d));
+        assertTrue(CMVCalculator.checkLIC1(points, 0.8d));
+    }
+
+    @Test
+    public void testFourPointsLIC1(){
+        float[] point1 = new float[]{ 1.5f, 3.0f};
+        float[] point2 = new float[]{ 1.5f, 0.0f};
+        float[] point3 = new float[]{ 0.0f, 1.5f};
+        float[] point4 = new float[]{ 5.5f, 3.0f};
+        float[][] points = new float[][]{point1, point2, point3, point4};
+
+        assertTrue(CMVCalculator.checkLIC1(points, 0.5d));
+        assertTrue(CMVCalculator.checkLIC1(points, 1.0d));
+        assertTrue(CMVCalculator.checkLIC1(points, 1.5d));
+        // edge case should be RADIUS = 5.0
+        assertFalse(CMVCalculator.checkLIC1(points, 5.0d));
+        assertFalse(CMVCalculator.checkLIC1(points, 5.1d));
+    }
 }
